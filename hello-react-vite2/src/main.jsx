9a5d8110f69c4896c1022bdd760b81fd0ad46b9d
lsy3709 <lsy3709@naver.com>
2026-04-28 15:12:44 +0900

    6교시, 생명주기 확인 준비 작업1

--- hello-react-vite2/src/main.jsx
+++ hello-react-vite2/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './ch06/App';
+import App from './ch07/App';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  <App />,
+  // </StrictMode>,
 );
