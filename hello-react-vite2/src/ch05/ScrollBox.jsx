f411299c6669d267a51480a60b762c1e0513e5e4
lsy3709 <lsy3709@naver.com>
2026-04-28 10:09:52 +0900

    2교시, 스크롤에 ref 달아서, 맨위, 가운데로 실습해보기

--- hello-react-vite2/src/ch05/ScrollBox.jsx
+++ hello-react-vite2/src/ch05/ScrollBox.jsx
@@ -4,9 +4,19 @@ class ScrollBox extends Component {
   // 순서2
   // 맨밑으로 이동하는 함수.
   scrollToBottom = () => {
+    /* 위 코드는 비구조화 할당 문법을 사용한 것입니다.
+           아래 코드는 같은 의미입니다:
+           scrollHeight : 650px
+           clientHeight : 300px
+           const scrollHeight = this.box.scrollHeight;
+           const clientHeight = this.box.clientHeight;
+        */
     const { scrollHeight, clientHeight } = this.box;
     this.box.scrollTop = scrollHeight - clientHeight;
   };
+
+  //scrollToTop , 으로 함수를 생성해서, 맨위로 올라가는 버튼을 만들어서 적용해보기.
+  //scrollToMiddle , 으로 함수를 생성해서, 가운데로 이동는 버튼을 만들어서 적용해보기.
 
   render() {
     return (
